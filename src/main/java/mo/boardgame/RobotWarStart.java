fc38be0cc07399727671458c652925a7d914f012
caojunqi <caojunqi@foxmail.com>
2021-12-23 11:13:11 +0800

    perf(框架): 代码抽象，减少重复代码。

--- src/main/java/mo/boardgame/RobotWarStart.java
+++ src/main/java/mo/boardgame/RobotWarStart.java
@@ -1,22 +1,25 @@
-package mo.boardgame.demo.gomoku;
+package mo.boardgame;
 
 import ai.djl.ndarray.NDManager;
+import mo.boardgame.game.BaseBoardGameEnv;
 import mo.boardgame.game.RobotWarEnv;
 
 import java.util.Random;
 
 /**
- * 五子棋机器人混战
+ * 机器人混战启动
  *
  * @author Caojunqi
- * @date 2021-12-08 22:25
+ * @date 2021-12-23 11:07
  */
-public class GomokuRobotWarTester {
+public class RobotWarStart {
 
     public static void main(String[] args) {
+        BoardGameType gameType = BoardGameType.GOMOKU;
+
         NDManager mainManager = NDManager.newBaseManager();
         Random random = new Random(0);
-        GomokuEnv gameEnv = new GomokuEnv(mainManager.newSubManager(), random, true);
+        BaseBoardGameEnv gameEnv = gameType.buildBoardGameEnv(mainManager.newSubManager(), random, true);
         RobotWarEnv robotWarEnv = new RobotWarEnv(mainManager.newSubManager(), random, gameEnv);
         robotWarEnv.run();
     }
